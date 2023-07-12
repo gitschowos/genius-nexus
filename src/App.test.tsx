50b6630a43cdc4c1906a9c7485fb13a3f4e84f36
Josef Rothballer <J.Rothballer@web.de>
2023-07-13 00:22:30 +0200

    Add pre-commit hook with husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders first welcome screen', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/welcome/i);
+  expect(headerElement).toBeInTheDocument();
 });
